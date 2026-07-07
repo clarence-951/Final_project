12c900dd03af14d83fbc19453c1acfa9d7999dc0
Twyman, Idries S. <ist00270@castleton.edu>
2026-07-07 14:20:05 -0400

    done basic

--- project/main.js
+++ project/main.js
@@ -2,26 +2,28 @@ import { drawGrid } from "@/utils/grid.js";
 import { drawAxes } from '@/utils/axes.js';
 import { vector } from "@/utils/vec3.js";
 
+
 //We can use this to load textures or sounds
 //export function preload() {
 
 let grid = []
 
-export function setup(){
-  camera(300, -200, 700);
-    grid =[
-    [2, 2, 2, 2, 2],
-    [2, 1, 1, 1, 2],
-    [2, 1, 3, 1, 1],
-    [2, 1, 1, 3, 2],
-    [2, 2, 2, 2, 2],
-];
+export function setup() {
+
+    camera(300, -200, 700);
+    grid = [
+        [2, 2, 2, 2, 2],
+        [2, 1, 1, 1, 2],
+        [2, 1, 3, 1, 1],
+        [2, 1, 1, 3, 2],
+        [2, 2, 2, 2, 2],
+    ];
 }
 
 let wall;
 let tile;
 let floor;
-
+let mySound
 export function preload() {
     let w = new URL(import.meta.url).pathname;
     w = w.substring(0, w.lastIndexOf("/") + 1);
@@ -32,45 +34,185 @@ export function preload() {
     let t = new URL(import.meta.url).pathname;
     t = t.substring(0, t.lastIndexOf("/") + 1);
     tile = loadImage(t + "panel.jpg");
-}
+    //soundFormats("mp3", 'ogg');
+    //mySound = loadSound('foxy jumpscare fnaf 2.mp3');
 
+
+}
 
 
 
 export function draw(t, dt) {
     background(30, 30, 30); //Clear the background to dark grey 
     orbitControl(); //Enable mouse movement in the scene
-    ambientLight(80, 80, 80);  //Add some ambient light to the scene
+    ambientLight(198, 197, 139);  //Add some ambient light to the scene
 
     directionalLight(255, 255, 255, 1, 1, -1); //Add a white directional light
+
+
 
     //drawGrid(); //Draw the grid
     drawAxes(); //Draw the axes
 
     push()
     translate(-500, 0, -500)
-    backroom()
+    // mySound.play()
+   // backroom()
     pop()
+bb7()
 
 }
 
 
 
 
-
-//terrain gen test
-
+//no walls
 function bb1() {
     let x = 0
     push()
     translate(0, 25, 0)
     texture(floor)
-    box(510, 1, 510); //Draw a box
+    box(525, 1, 525); //Draw a box
     pop()
     push()
     translate(0, -325, 0)
     texture(tile)
-    box(510, -1, 510);
+    box(525, 1, 525);
+    pop()
+
+}
+//-x
+function bb2() {
+    let x = 0
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    rotateY(270)
+    while (x < 3) {
+
+        push()
+        translate(250, -150, 0)
+        texture(wall);
+        box(20, 350, 521)
+        rotateY(-180)
+        translate(-1, 0)
+        box(20, 350, 521.5)
+        pop()
+        rotateY(90)
+        x = x + 1
+    }
+    pop()
+
+}
+//-z
+function bb3() {
+    let x = 0
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    rotateY(180)
+    while (x < 3) {
+
+         push()
+        translate(250, -150, 0)
+        texture(wall);
+        box(20, 350, 521)
+        rotateY(-180)
+        translate(-1, 0)
+        box(20, 350, 521.5)
+        pop()
+        rotateY(90)
+        x = x + 1
+    }
+    pop()
+
+}
+//x
+function bb4() {
+   let x = 0
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    rotateY(90)
+    while (x < 3) {
+
+         push()
+        translate(250, -150, 0)
+        texture(wall);
+        box(20, 350, 521)
+        rotateY(-180)
+        translate(-1, 0)
+        box(20, 350, 521.5)
+        pop()
+        rotateY(90)
+        x = x + 1
+    }
+    pop()
+}
+//z
+function bb5() {
+    let x = 0
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    while (x < 3) {
+
+         push()
+        translate(250, -150, 0)
+        texture(wall);
+        box(20, 350, 521)
+        rotateY(-180)
+        translate(-1, 0)
+        box(20, 350, 521.5)
+        pop()
+        rotateY(90)
+        x = x + 1
+    }
+    pop()
+
+}
+//door
+function bb6() {
+   let x = 0
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
     pop()
     push()
     while (x < 4) {
@@ -80,6 +222,9 @@ function bb1() {
         texture(wall);
         //box(20, 350, 250)
         doorway()
+        rotateY(-180)
+        translate(-1, 0)
+        doorway()
         pop()
         rotateY(90)
         x = x + 1
@@ -87,17 +232,17 @@ function bb1() {
     pop()
 }
 
-function bb2() {
-    let x = 0
+function bb7() {
+let x = 0
     push()
     translate(0, 25, 0)
     texture(floor)
-    box(510, 1, 510); //Draw a box
+    box(525, 1, 525); //Draw a box
     pop()
     push()
     translate(0, -325, 0)
     texture(tile)
-    box(510, -1, 510);
+    box(525, 1, 525);
     pop()
     push()
     while (x < 4) {
@@ -105,7 +250,10 @@ function bb2() {
         push()
         translate(250, -150, 0)
         texture(wall);
-        box(20, 350, 490)
+        box(20, 350, 521)
+        rotateY(-180)
+        translate(-1, 0)
+        box(20, 350, 521.5)
         pop()
         rotateY(90)
         x = x + 1
@@ -113,53 +261,90 @@ function bb2() {
     pop()
 }
 
-function bb3() {
-    let x = 0
-    push()
-    translate(0, 25, 0)
-    texture(floor)
-    box(510, 1, 510); //Draw a box
-    pop()
-    push()
-    translate(0, -325, 0)
-    texture(tile)
-    box(510, -1, 510);
-    pop()
 
-}
 
 function backroom() {
     for (let x = 0; x < 5; x++) {
 
         for (let z = 0; z < 5; z++) {
             push()
-            translate(x * 505, 0, z * 505)
-            if (grid[x][z] == 0
-            ) { sphere(.1) }
+            translate(x * 521, 0, z * 521)
+            if (grid[x][z] == 0) {
+            }
             if (grid[x][z] == 1) {
+                fill(0, 250, 0)
                 bb1()
             }
             if (grid[x][z] == 2) {
                 bb2()
             }
             if (grid[x][z] == 3) {
-                fill(0, 250, 0)
                 bb3()
             }
+            if (grid[x][z] == 4) {
+                bb4()
+            }
+            if (grid[x][z] == 5) {
+                bb5()
+            }
+            if (grid[x][z] == 6) {
+                bb6()
+            }
+             if (grid[x][z] == 7) {
+                bb7()
+            }
+
             pop()
         }
     }
 }
 
+
 function doorway() {
     texture(wall)
     push()
     translate(0, 0, 190)
-    box(20, 350, 130)
+    box(20, 350, 141.5)
     pop()
     push()
     translate(0, 0, -190)
-    box(20, 350, 130)
+    box(20, 350, 141.5)
     pop()
 }
 function async() { }
+
+function bbox() {
+    beginShape()
+    vertex(10, 0, -10)
+    vertex(-10, 0, -10)
+    vertex(10, 0, 10)
+    endShape()
+    beginShape()
+    vertex(10, 0, 10)
+    vertex(10, 0, -10)
+    vertex(-10, 0, -10)
+    endShape()
+    beginShape()
+    vertex(10, 10, -10)
+    vertex(-10, 10, -10)
+    vertex(10, 10, 10)
+    endShape()
+    beginShape()
+    vertex(10, 10, 10)
+    vertex(10, 10, -10)
+    vertex(-10, 10, -10)
+    endShape()
+    beginShape()
+    vertex(10, 10, -10)
+    vertex(10, 10, 10)
+    vertex(-10, 0, -10)
+    endShape()
+    beginShape()
+    vertex(10, 10, -10)
+    vertex(10, 0, -10)
+    vertex()
+    endShape()
+
+}
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import path from 'path'
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   // Show a full-screen error overlay in the browser whenever
   // there is a JavaScript error — students see problems immediately.
+    plugins: [
+    inject({
+      p5: 'p5',
+    }),
+  ],
   server: {
     overlay: true,
     host: '0.0.0.0',
