50edf8e59dc07bacae687b6b090a1d42369889dd
Twyman, Idries S. <ist00270@castleton.edu>
2026-07-08 12:01:45 -0400

    back begins

--- project/main.js
+++ project/main.js
@@ -10,15 +10,29 @@ let grid = []
 
 export function setup() {
 
-    camera(0,-200, 50,0,-100,1000);
+    camera(0, -200, 50, 0, -100, 1000);
     grid = [
-        [4, 5, 6, 6, 2, 4, 6],
-        [3, 6, 2, 4, 1, 5, 3],
-        [2, 1, 1, 6, 2, 3, 1],
-        [1, 6, 2, 1, 1, 2, 1],
-        [4, 1, 1, 2, 2, 1, 3],
-        [7, 6, 1, 5, 2, 2, 5],
-        [6, 6, 2, 1, 5, 2, 7],
+        [4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 , 10, 10, 10, 10, 10, 10, 7],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11],
+        [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [2, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 12],
+
     ];
 }
 
@@ -61,12 +75,12 @@ export function draw(t, dt) {
     drawAxes(); //Draw the axes
 
     push()
-    translate(-1560, 0, 500)
-    
+    translate(-1563, 0, 530)
+
     backroom()
     pop()
     threshold()
-    
+
 }
 
 
@@ -76,13 +90,75 @@ export function draw(t, dt) {
 
 function threshold() {
     push()
-    translate(0,20,-11)
-    fill(80,80,80)
-    box(500,20,500)
+    translate(0, 20, -11)
+    fill(80, 80, 80)
+    box(500, 20, 600)
+    pop()
+    push()
+    for (let x = 0; x < 3; x++) {
+
+        push()
+        translate(250, -150, 0)
+        fill(100, 100, 100)
+        box(25, 350, 500)
+        pop()
+        rotateY(90)
+
+    }
+    pop()
+    push()
+    rotateY(90)
+    translate(-280, -150,)
+    doorway()
+    push()
+    translate(28, 0, -180)
+    fill(90, 90, 90)
+    box(30, 350, 150)
+    pop()
+    push()
+    translate(28, 0, 180)
+    fill(90, 90, 90)
+    box(30, 350, 150)
+    pop()
+    push()
+    translate(28, 10, -180)
+    rotateY(-24)
+    fill(90, 90, 90)
+    box(30, 310, 140)
+    pop()
+    push()
+    translate(28, 10, 180)
+    rotateY(24)
+    fill(90, 90, 90)
+    box(30, 310, 140)
+    pop()
+    pop()
+    push()
+    translate(0, -625)
+    rotateX(180)
+    push()
+    translate(0, 20, -11)
+    fill(80, 80, 80)
+    box(500, 20, 500)
+    pop()
+    push()
+    for (let x = 0; x < 4; x++) {
+
+        push()
+        translate(250, -150, 0)
+        fill(100, 100, 100)
+        box(30, 350, 500)
+        pop()
+        rotateY(90)
+
+    }
+    pop()
     pop()
 
-    
 }
+
+
+
 function bb1() {
     let x = 0
     push()
@@ -237,7 +313,7 @@ function bb6() {
     }
     pop()
 }
-
+//4walls
 function bb7() {
     let x = 0
     push()
@@ -263,16 +339,167 @@ function bb7() {
     }
     pop()
 }
+//1 wall
+function bb8() {
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    translate(250, -150, 0)
+    texture(wall);
+    backwall(20, 350, 521)
 
+    pop()
+}
+//1wall
+function bb9() {
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    rotateY(90)
+    translate(250, -150, 0)
+    texture(wall);
+    backwall(20, 350, 521)
 
+    pop()
+}
+//1wall
+function bb10() {
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    rotateY(180)
+    translate(250, -150, 0)
+    texture(wall);
+    backwall(20, 350, 521)
+
+    pop()
+}
+//1wall
+function bb11() {
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    rotateY(270)
+    translate(250, -150, 0)
+    texture(wall);
+    backwall(20, 350, 521)
+
+    pop()
+}
+//long room
+function bb12() {
+push()
+translate(-269,0)
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(1025, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(1025, 1, 525);
+    pop()
+    push()
+    rotateY(270)
+    translate(250, -150, 252)
+    texture(wall);
+    backwall(20, 350, 521)
+    translate(0, 0, -516)
+    backwall(20, 350, 510)
+    pop()
+    push()
+    rotateY(-270)
+    translate(250, -150, 252)
+    texture(wall);
+    backwall(20, 350, 521)
+    translate(0, 0, -516)
+    backwall(20, 350, 510)
+    pop()
+    push()
+    translate(510, -150)
+    backwall(20, 350, 521)
+    pop()
+    push()
+    translate(250, -150, 0)
+    texture(wall);
+
+    translate(-50, 100,)
+    backwall(20, 150, 500)
+    translate(0, -70)
+    fill(150, 75, 0)
+    box(30, 20, 500)
+
+    pop()
+    pop()
+}
+
+//halfwall
+function bb13() {
+    push()
+    translate(0, 25, 0)
+    texture(floor)
+    box(525, 1, 525); //Draw a box
+    pop()
+    push()
+    translate(0, -325, 0)
+    texture(tile)
+    box(525, 1, 525);
+    pop()
+    push()
+    translate(250, -150, 0)
+    texture(wall);
+    backwall(20, 350, 521)
+    rotateY(90)
+    translate(-50, 100, -150)
+    backwall(20, 150, 300)
+    translate(0, -70)
+    fill(150, 75, 0)
+    box(30, 20, 310)
+
+    pop()
+}
 
 function backroom() {
-    for (let x = 0; x < 7; x++) {
+    for (let x = 0; x < 20; x++) {
 
-        for (let z = 0; z < 7; z++) {
+        for (let z = 0; z < 20; z++) {
             push()
             translate(x * 521, 0, z * 521)
             if (grid[x][z] == 0) {
+                sphere(.0001)
             }
             if (grid[x][z] == 1) {
                 fill(0, 250, 0)
@@ -295,6 +522,24 @@ function backroom() {
             }
             if (grid[x][z] == 7) {
                 bb7()
+            }
+            if (grid[x][z] == 8) {
+                bb8()
+            }
+            if (grid[x][z] == 9) {
+                bb9()
+            }
+            if (grid[x][z] == 10) {
+                bb10()
+            }
+            if (grid[x][z] == 11) {
+                bb11()
+            }
+            if (grid[x][z] == 12) {
+                bb12()
+            }
+            if (grid[x][z] == 13) {
+                bb13()
             }
 
             pop()
