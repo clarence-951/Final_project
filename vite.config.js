88d26626f74f53d5227d4e99ad4d49922fa444b7
Twyman, Idries S. <ist00270@castleton.edu>
2026-07-09 14:36:16 -0400

    fix this thing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import inject from '@rollup/plugin-inject';
 import path from 'path'
-import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   // Show a full-screen error overlay in the browser whenever
   // there is a JavaScript error — students see problems immediately.
-    plugins: [
-    inject({
-      p5: 'p5',
-    }),
-  ],
   server: {
     overlay: true,
     host: '0.0.0.0',
